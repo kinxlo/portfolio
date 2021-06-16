996776d5acb5f1f0e795ebbd456e8e55856e19f4
Kingsley Solomon <kinxly@gmail.com>
2021-06-16 21:02:42 +0100

    fixed skills

--- js/app.js
+++ js/app.js
@@ -119,7 +119,7 @@ const ABOUT = `
                         <div class="img-container">
                             <img src="./assets/images/center-img5.png" alt="Kingsley Solomon">
                         </div>
-                        <p id="about-me"></p>
+                        <p id="about-me" class='shadow'></p>
                     </div>
                     <h3 class='shadow'>What you would be getting</h3>
                 </div>
@@ -176,7 +176,7 @@ const CONTACT = `
                             <textarea name="" id="" placeholder="Message" required></textarea>
                         </div>
                         </section>
-                        <svg class='neu-light' xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill:rgba(0, 0, 0, 1);transform:;-ms-filter:"><path d="M21.426,11.095l-17-8c-0.35-0.163-0.763-0.112-1.061,0.133C3.066,3.473,2.937,3.868,3.03,4.242L4.969,12L3.03,19.758 c-0.094,0.374,0.036,0.77,0.335,1.015C3.548,20.923,3.772,21,4,21c0.145,0,0.29-0.031,0.426-0.095l17-8 C21.776,12.74,22,12.388,22,12S21.776,11.26,21.426,11.095z M5.481,18.197L6.32,14.84L12,12L6.32,9.16L5.481,5.803L18.651,12 L5.481,18.197z"></path></svg>
+                        <button><svg class='neu-light' xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill:rgba(0, 0, 0, 1);transform:;-ms-filter:"><path d="M21.426,11.095l-17-8c-0.35-0.163-0.763-0.112-1.061,0.133C3.066,3.473,2.937,3.868,3.03,4.242L4.969,12L3.03,19.758 c-0.094,0.374,0.036,0.77,0.335,1.015C3.548,20.923,3.772,21,4,21c0.145,0,0.29-0.031,0.426-0.095l17-8 C21.776,12.74,22,12.388,22,12S21.776,11.26,21.426,11.095z M5.481,18.197L6.32,14.84L12,12L6.32,9.16L5.481,5.803L18.651,12 L5.481,18.197z"></path></svg></button>
                 </form>
                 <div class="arrow-img">
                     <h6>you can also find me here.</h6>
